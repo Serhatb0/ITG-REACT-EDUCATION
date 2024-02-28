b6fcb7bd5dbbb0869dcc2026386c2e89895687fb
Serhatb0 <77425377+Serhatb0@users.noreply.github.com>
2024-02-28 14:55:06 +0300

    'CHALLENGE-2'

--- challenge-2/src/pages/Categories/add/index.tsx
+++ challenge-2/src/pages/Categories/add/index.tsx
@@ -1,79 +1,80 @@
-import { Button, CircularProgress, TextField } from '@mui/material'
-import React from 'react'
-import { useForm } from 'react-hook-form';
-import axiosInstance from '../../../config/axiosInstance';
-import { useMutation } from 'react-query';
+import { Button, CircularProgress, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
+import axiosInstance from "../../../config/axiosInstance";
+import { useMutation } from "react-query";
 
 function CategoryAdd() {
-
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset
+    reset,
   } = useForm();
 
-  const onSubmit = (data:any) => {
-          let newCategory = {
-            name: data.name,
-            description: data.description,
-          };
+  const onSubmit = (data: any) => {
+    let newCategory = {
+      name: data.name,
+      description: data.description,
+    };
 
-          handleAddCategory(newCategory);
+    handleAddCategory(newCategory);
   };
 
-
   const addCategoryMutation = useMutation(
-    (newOrder:any) => axiosInstance.post('categories', newOrder),
+    (newCategory: any) => axiosInstance.post("category", newCategory),
     {
       onSuccess: () => {
-        reset(); 
+        reset();
       },
       onError: (error) => {
-        console.error('Add order mutation error:', error)
+        console.error("Add Category mutation error:", error);
       },
     }
   );
 
-const handleAddCategory = async (newCategory:any) => {
-  try {
-    await addCategoryMutation.mutateAsync(newCategory);
-  } catch (error) {
-    console.error('Add order mutation error:', error);
-  }
-};
-
+  const handleAddCategory = async (newCategory: any) => {
+    try {
+      await addCategoryMutation.mutateAsync(newCategory);
+    } catch (error) {
+      console.error("Add Category mutation error:", error);
+    }
+  };
 
   return (
-     <form onSubmit={handleSubmit(onSubmit)}>
-      <TextField 
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField
         label="name"
         variant="outlined"
         fullWidth
         margin="normal"
-        {...register("name",{required:true,maxLength:10})}
+        {...register("name", { required: true, maxLength: 10 })}
       />
-      {errors.name?.type ==="required" && <span>This field is required</span>}
-      {errors.name?.type ==="maxLength" && <span>maxLength 10</span>}
+      {errors.name?.type === "required" && <span>This field is required</span>}
+      {errors.name?.type === "maxLength" && <span>maxLength 10</span>}
       <TextField
         label="description"
         variant="outlined"
         fullWidth
         margin="normal"
         {...register("description")}
-     
       />
-       {errors.description?.type ==="required" && <span>This field is required</span>}
-       <Button
-      variant="contained"
-      color="primary"
-      type="submit"
-      disabled={addCategoryMutation.isLoading} // İşlem devam ederken düğmeyi devre dışı bırak
-    >
-      {addCategoryMutation.isLoading ? <CircularProgress size={24} /> : "Submit"}
-    </Button>
+      {errors.description?.type === "required" && (
+        <span>This field is required</span>
+      )}
+      <Button
+        variant="contained"
+        color="primary"
+        type="submit"
+        disabled={addCategoryMutation.isLoading} // İşlem devam ederken düğmeyi devre dışı bırak
+      >
+        {addCategoryMutation.isLoading ? (
+          <CircularProgress size={24} />
+        ) : (
+          "Submit"
+        )}
+      </Button>
     </form>
-  )
+  );
 }
 
-export default CategoryAdd
+export default CategoryAdd;
